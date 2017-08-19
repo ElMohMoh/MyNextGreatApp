75726e87e3c022529def362fe86f658009600866
ElMohMoh <chaiebm8@gmail.com>
2017-08-19 19:30:17 +0200

    segmented bar

--- platforms/android/src/main/assets/app/page/main-page/main-page.ts
+++ platforms/android/src/main/assets/app/page/main-page/main-page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ItemService } from "../../item/item.service";
 import { Session } from "../../shared/interfaces";
+import { SegmentedBar, SegmentedBarItem } from "ui/segmented-bar";
 
 @Component({
     selector: "mp",
@@ -8,9 +9,21 @@ import { Session } from "../../shared/interfaces";
     templateUrl: "./main-page.html",
 })
 export class MainPage implements OnInit {
-    myItems: Object[]; 
 
-    constructor(private itemService: ItemService) { }
+    myItems: Object[];
+    public confDayOptions: Array<SegmentedBarItem>;
+    public selectedIndex: number;
+    public dayHeader: string;
+
+    constructor(private itemService: ItemService) { 
+        this.confDayOptions = [];
+        for (let i = 1; i < 4; i++) {
+            let segmentedBarItem = <SegmentedBarItem>new SegmentedBarItem();
+            segmentedBarItem.title = "day " + i;
+            this.confDayOptions.push(segmentedBarItem);
+        }
+        this.selectedIndex = 0;
+    }
 
     ngOnInit(): void {
         this.myItems = this.itemService.getSessions();
@@ -18,5 +31,11 @@ export class MainPage implements OnInit {
 
     toggleFavorite(args): void {
         args.favorite = !args.favorite;
+    }
+
+    onSelectedIndexChange(args): void{
+        let segmetedBar = <SegmentedBar>args.object;
+        let day: SegmentedBarItem = this.confDayOptions[segmetedBar.selectedIndex];
+        this.dayHeader = day.title;
     }
 }
